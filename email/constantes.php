fd9a0240d6e128318d85e0b5cbb7f749304964f2
Miguel Herrera <121081107+MHerrera94@users.noreply.github.com>
2024-05-13 02:25:46 -0500

    actualizacion de datos y enviar email

---
+++ email/constantes.php
@@ -0,0 +1,15 @@
+<?php
+
+define('EMAIL_HOST', 'smtp.gmail.com');
+define('EMAIL_PORT', 465);
+define('EMAIL_NAME', 'gokuherr@gmail.com');
+define('EMAIL_PASS', 'stfg htpm moxe rjxj');
+define('SITE_NAME', 'Portafolio Contacto');
+
+# ⚠⚠⚠ LEA CON CUIDADO ⚠⚠⚠
+
+// PAGINA PRINCIPAL PARA EMAIL NO MODIFIQUE A MENOS QUE SEPA LO QUE ESTA HACIENDO.
+
+// © ANGELUS INFERNUS - FOR SAORI CODER
+
+# ⚠⚠⚠ DO NOT DELETE ⚠⚠⚠
